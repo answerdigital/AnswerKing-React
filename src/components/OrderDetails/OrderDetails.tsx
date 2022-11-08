52db346b743e19b37b08193b2025889400f30153
hazel-davis <hazel.davis@answerdigital.com>
2022-11-08 10:57:48 +0000

    reset changes to order details (2)

--- src/components/OrderDetails/OrderDetails.tsx
+++ src/components/OrderDetails/OrderDetails.tsx
@@ -20,19 +20,16 @@ export const OrderDetails = ({ order }: Props): ReactElement => {
       <div className="order_details__group order_details__group--items">
         <span className="order_details__label">Items:</span>
         <div id="order_details__items">
-          {!order.items?.length ? (
-            <p>There are no items in your order.</p>
-          ) : (
-            order.items?.map((item) => (
-              <div className="order_details__item" key={item.id}>
-                <div className="order_details__item_group">
-                  <span className="order_details__item_quantity">{item.quantity}x</span>
-                  <span className="order_details__item_name">{item.name}</span>
-                </div>
-                <span className="order_details__item_price">£{(item.price * item.quantity).toFixed(2)}</span>
+          {order.items.length === 0 ? <p>There are no items in your order.</p> : null}
+          {order.items.map((item) => (
+            <div className="order_details__item" key={item.id}>
+              <div className="order_details__item_group">
+                <span className="order_details__item_quantity">{item.quantity}x</span>
+                <span className="order_details__item_name">{item.name}</span>
               </div>
-            ))
-          )}
+              <span className="order_details__item_price">£{(item.price * item.quantity).toFixed(2)}</span>
+            </div>
+          ))}
         </div>
       </div>
 
