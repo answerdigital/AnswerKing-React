0cf489dd62597fcda25f85a88a1e9d3dd55a3ef0
hazel-davis <hazel.davis@answerdigital.com>
2022-11-09 16:01:59 +0000

    re-modified order orderdetails to re-add null protection

--- src/components/OrderDetails/OrderDetails.tsx
+++ src/components/OrderDetails/OrderDetails.tsx
@@ -20,16 +20,19 @@ export const OrderDetails = ({ order }: Props): ReactElement => {
       <div className="order_details__group order_details__group--items">
         <span className="order_details__label">Items:</span>
         <div id="order_details__items">
-          {order.items?.length > 0 ? null : <p>There are no items in your order.</p>}
-          {order.items?.map((item) => (
-            <div className="order_details__item" key={item.id}>
-              <div className="order_details__item_group">
-                <span className="order_details__item_quantity">{item.quantity}x</span>
-                <span className="order_details__item_name">{item.name}</span>
+          {order.lineItems?.length ? (
+            order.lineItems.map((lineItem) => (
+              <div className="order_details__item" key={lineItem.product.id}>
+                <div className="order_details__item_group">
+                  <span className="order_details__item_quantity">{lineItem.quantity}x</span>
+                  <span className="order_details__item_name">{lineItem.product.name}</span>
+                </div>
+                <span className="order_details__item_price">£{lineItem.subTotal}</span>
               </div>
-              <span className="order_details__item_price">£{(item.price * item.quantity).toFixed(2)}</span>
-            </div>
-          ))}
+            ))
+          ) : (
+            <p>There are no items in your order.</p>
+          )}
         </div>
       </div>
 
