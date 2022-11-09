d56536e04e8bb13acde1916eb54cf74545888c16
SWhyteAnswer <116073430+SWhyteAnswer@users.noreply.github.com>
2022-11-09 15:38:27 +0000

    fixed merge

--- src/components/OrderPanel/OrderPanel.tsx
+++ src/components/OrderPanel/OrderPanel.tsx
@@ -1,56 +1,3 @@
-<<<<<<< HEAD
-import './OrderPanel.scss';
-import { Button } from 'components/Button/Button';
-import { OrderCreateForm } from 'components/OrderCreateForm/OrderCreateForm';
-import { OrderDetails } from 'components/OrderDetails/OrderDetails';
-import { OrderLoadForm } from 'components/OrderLoadForm/OrderLoadForm';
-import { useOrder } from 'hooks/useOrder';
-import { ReactElement } from 'react';
-
-interface Props {
-  toggleOrderPanel(): void;
-}
-
-export const OrderPanel = ({ toggleOrderPanel }: Props): ReactElement => {
-  const { order, clearOrder } = useOrder();
-
-  const handleClear = (): void => clearOrder();
-
-  return (
-    <div className="order_panel">
-      <div
-        className="order_panel__close"
-        onClick={toggleOrderPanel}
-        onKeyDown={toggleOrderPanel}
-        role="button"
-        tabIndex={0}
-      >
-        &times;
-      </div>
-      <h1>Order</h1>
-      {order.data ? (
-        <>
-          <OrderDetails order={order.data} />
-          <div className="order_panel__button_group">
-            <Button className="order_panel__button" onClick={handleClear}>
-              Cancel
-            </Button>
-            <Button active className="order_panel__button" onClick={handleClear}>
-              Checkout
-            </Button>
-          </div>
-        </>
-      ) : (
-        <>
-          <OrderLoadForm />
-          <hr />
-          <OrderCreateForm />
-        </>
-      )}
-    </div>
-  );
-};
-=======
 import './OrderPanel.scss';
 import { Button } from 'components/Button/Button';
 import { OrderCreateForm } from 'components/OrderCreateForm/OrderCreateForm';
@@ -103,4 +50,3 @@ export const OrderPanel = ({ toggleOrderPanel }: Props): ReactElement => {
     </div>
   );
 };
->>>>>>> 7ae8122 (reworked palette)
