9abb673a793efe33211e262e66429fbb77277eba
Harry Stead <Harry.Stead@mft.nhs.uk>
2022-11-23 11:50:13 +0000

    BENCH-210: Create order working

--- src/components/OrderCreateForm/OrderCreateForm.tsx
+++ src/components/OrderCreateForm/OrderCreateForm.tsx
@@ -5,6 +5,7 @@ import { LoaderOverlay } from 'components/LoaderOverlay/LoaderOverlay';
 import { useOrder } from 'hooks/useOrder';
 import { FormEvent, ReactElement, useState } from 'react';
 import { OrderCreateDto } from 'dtos/OrderCreateDto';
+import { orderService } from '../../services/orderService';
 
 interface Props {
   state: OrderCreateDto;
@@ -12,36 +13,19 @@ interface Props {
 
 export const OrderCreateForm = ({state}: Props): ReactElement => {
   const [validationMessage, setValidationMessage] = useState('');
-  const { createOrder } = useOrder();
+  const { createOrder, order} = useOrder();
 
-  const handleSubmit = (event: FormEvent<HTMLFormElement>): void => {
+  const handleSubmit = (event: FormEvent): void => {
     event.preventDefault();
-    setValidationMessage('');
     const orderCreateDto: OrderCreateDto = { lineItems: state.lineItems };
     createOrder.mutate(orderCreateDto);
-  };
-
-  const handleErrorClear = (): void => {
-    setValidationMessage('');
-    createOrder.reset();
   };
 
   return (
     <form className="order_create_form" onSubmit={handleSubmit}>
       <LoaderOverlay isEnabled={createOrder.isLoading} />
-      <p className="order_create_form__description">Create a new order</p>
-      {createOrder.error ? (
-        <Error onClear={handleErrorClear}>
-          <li>{createOrder.error.title}</li>
-        </Error>
-      ) : null}
-      {validationMessage ? (
-        <Error onClear={handleErrorClear}>
-          <li>{validationMessage}</li>
-        </Error>
-      ) : null}
       <Button className="order_create_form__button" type="submit">
-        Create Order
+        Checkout
       </Button>
     </form>
   );
--- src/components/OrderPanel/OrderPanel.tsx
+++ src/components/OrderPanel/OrderPanel.tsx
@@ -4,37 +4,32 @@ import { OrderCreateForm } from 'components/OrderCreateForm/OrderCreateForm';
 import { OrderDetails } from 'components/OrderDetails/OrderDetails';
 import { OrderLoadForm } from 'components/OrderLoadForm/OrderLoadForm';
 import { useOrder } from 'hooks/useOrder';
-import { ReactElement } from 'react';
+import { FormEvent, ReactElement } from 'react';
 import { OrderCreateDto } from 'dtos/CreatedOrderDto';
 
 interface Props {
   state: OrderCreateDto;
 }
 
-export const OrderPanel = ({state}: Props): ReactElement => {
+export const OrderPanel = ({ state }: Props): ReactElement => {
   const { order, clearOrder } = useOrder();
 
   const handleClear = (): void => clearOrder();
 
+  const submit = (event: FormEvent): void => {
+    event.preventDefault();
+    console.log('submit');
+  };
   return (
     <div className="order_panel">
       {state.lineItems.length > 0 ? (
         <>
           <OrderDetails state={state} />
-          <div className="order_panel__button_group">
-            <Button className="order_panel__button">
-              Cancel
-            </Button>
-            <Button active className="order_panel__button">
-              Checkout
-            </Button>
-          </div>
+          <OrderCreateForm state={state} />
         </>
       ) : (
         <>
-          <OrderLoadForm />
-          <hr />
-          <OrderCreateForm state={state}/>
+          <OrderCreateForm state={state} />
         </>
       )}
     </div>
