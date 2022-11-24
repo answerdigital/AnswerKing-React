40a09c499661123e34cf7ea01b986fbf6fe817a6
SWhyteAnswer <116073430+SWhyteAnswer@users.noreply.github.com>
2022-11-24 12:59:24 +0000

    remove dummy data

--- src/services/categoryService.ts
+++ src/services/categoryService.ts
@@ -1,50 +1,18 @@
 import { CategoryDto } from 'dtos/CategoryDto';
-import { stringify } from 'querystring';
 import { httpClient } from 'utilities/http-client';
 
 const getAll = async (): Promise<CategoryDto[]> => {
   const response = await httpClient.get('/categories');
 
-  // if (!response.ok) {
-  //   try {
-  //     return Promise.reject(await response.json());
-  //   } catch {
-  //     return Promise.reject();
-  //   }
-  // }
+  if (!response.ok) {
+    try {
+      return Promise.reject(await response.json());
+    } catch {
+      return Promise.reject();
+    }
+  }
 
-  // return await response.json();
-
-  return dummyData();
-};
-
-const dummyData = (): any => {
-
-  const Burgers: CategoryDto = {
-    id: 1,
-    name: 'Burger',
-    description: 'juicy burgers'
-  };
-
-  const Sides: CategoryDto = {
-    id: 2,
-    name: 'Sides',
-    description: 'Small side orders'
-  };
-
-  const Drinks: CategoryDto = {
-    id: 3,
-    name: 'Drinks',
-    description: 'Drinkable liquids'
-  };
-
-  const _categories: CategoryDto[] = [];
-  
-  _categories.push(Burgers);
-  _categories.push(Sides);
-  _categories.push(Drinks);
-
-  return _categories;
+  return await response.json();
 };
 
 export const categoryService = { getAll };
