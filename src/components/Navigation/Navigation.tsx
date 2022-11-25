6799a8f2fd587dae6898a6a5fbeb10143281cd6d
Harry Stead <Harry.Stead@mft.nhs.uk>
2022-11-25 09:29:40 +0000

    BENCH-216: Comments implemented

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -28,12 +28,6 @@ export const Navigation = (): ReactElement => {
         >
           Staff
         </NavLink>
-        <NavLink
-          className={({ isActive }) => cn('nav__item', { 'nav__item--active': isActive })}
-          to={RouteConstants.CHECKOUT}
-        >
-          Checkout
-        </NavLink>
       </div>
       <div className="nav__section">
         <img alt="Logo" className="nav__logo" src={AnswerLogo} />
