733264406dcc7fa13a2f23f2688eea9a7184fc9c
Harry Stead <Harry.Stead@mft.nhs.uk>
2022-12-02 14:00:44 +0000

    BENCH-214: Filter active products

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -4,7 +4,6 @@ import { httpClient } from 'utilities/http-client';
 
 const getAll = async (): Promise<ProductDto[]> => {
   const response = await httpClient.get('/products');
-
   if (!response.ok) {
     try {
       return Promise.reject(await response.json());
