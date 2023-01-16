dba7c510389b0854763b00b0260fd5cfe65d6190
NogaAD <115239141+NogaAD@users.noreply.github.com>
2023-01-16 13:38:40 +0000

    Added contact info & changed text font & colour

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -8,8 +8,12 @@ export const Navigation = (): ReactElement => {
         <a href="/" className="ml-16">
           <img src={AnswerLogo} className="h-[60.18px] w-[170px]"></img>
         </a>
-        <div className="mr-16 md:order-2">
-          <button className="rounded-[25px] bg-[#A2AAB6] py-[8px] px-[40px] text-black hover:cursor-pointer">Log in</button>
+
+        <div className="mr-16 md:order-2 flex flex-row items-center gap-x-8">
+          <div className="text-white-base-center font-light font-poppins">
+            Contact: 07936 286319
+          </div>
+          <button className="rounded-[25px] bg-[#A2AAB6] py-[8px] px-[40px] text-[#333F4C] font-poppins hover:cursor-pointer">Log In</button>
         </div>
       </div>
     </nav>
