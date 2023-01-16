80b49c74ed6743928a5634ecad95f63408a6c2c4
NogaAD <115239141+NogaAD@users.noreply.github.com>
2023-01-16 13:41:33 +0000

    formatting

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -9,11 +9,9 @@ export const Navigation = (): ReactElement => {
           <img src={AnswerLogo} className="h-[60.18px] w-[170px]"></img>
         </a>
 
-        <div className="mr-16 md:order-2 flex flex-row items-center gap-x-8">
-          <div className="text-white-base-center font-light font-poppins">
-            Contact: 07936 286319
-          </div>
-          <button className="rounded-[25px] bg-[#A2AAB6] py-[8px] px-[40px] text-[#333F4C] font-poppins hover:cursor-pointer">Log In</button>
+        <div className="mr-16 flex flex-row items-center gap-x-8 md:order-2">
+          <div className="text-white-base-center font-poppins font-light">Contact: 07936 286319</div>
+          <button className="font-poppins rounded-[25px] bg-[#A2AAB6] py-[8px] px-[40px] text-[#333F4C] hover:cursor-pointer">Log In</button>
         </div>
       </div>
     </nav>
