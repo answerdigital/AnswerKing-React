d0797a290b30862dd097c5d6bf8d6e90c18c5514
AudriusPutys <Audrius.Putys@answerdigital.com>
2023-01-17 09:37:02 +0000

    BENCH-262 home page update, footer added

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,13 @@
-import {ReactElement} from 'react';
-import {useNavigate} from 'react-router-dom';
-import {RouteConstants} from 'utilities/route-constants';
+import { ReactElement } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { RouteConstants } from 'utilities/route-constants';
 
 export const Footer = (): ReactElement => {
   const navigate = useNavigate();
 
   return (
     <>
-      <div className="grid grid-cols-3 gap-3 mt-20 justify-items-center text-gray-400">
+      <div className="mt-20 grid grid-cols-3 justify-items-center gap-3 text-gray-400">
         <a className="cursor-pointer hover:text-white" onClick={() => navigate(RouteConstants.ALLERGEN_BOARD)}>
           View Allergen board
         </a>
