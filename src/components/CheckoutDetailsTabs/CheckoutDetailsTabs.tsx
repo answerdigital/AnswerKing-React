c3f364b4d092ba7f32a6e075cae380572d61f3a3
harrystead2 <88429620+harrystead2@users.noreply.github.com>
2023-01-24 14:03:42 +0000

    BENCH-244: Implemented New Design for Checkout With No Items (#96)
    * BENCH-244: Added new design for checkout no line items
    
    * BENCH-244: Refactored checkout tabs
    
    * BENCH-244: changed conditional name
    
    Co-authored-by: Harry Stead <Harry.Stead@mft.nhs.uk>

--- src/components/CheckoutDetailsTabs/CheckoutDetailsTabs.tsx
+++ src/components/CheckoutDetailsTabs/CheckoutDetailsTabs.tsx
@@ -1,17 +1,19 @@
 import { ReactElement } from 'react';
 import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import cn from 'classnames';
 
 export const CheckoutDetailsTabs = (): ReactElement => {
+  const tabElement = 'mx-5 self-center';
   return (
-    <div className="mb-10 flex text-[18px] font-[300] leading-[24px]">
-      <div className="mx-5 self-center rounded-full bg-[#A2AAB6] px-4 py-1 text-[#333F4C] shadow-md">Order</div>
-      <FontAwesomeIcon icon={faAngleRight} className="mx-5 self-center" />
-      <div className="mx-5 self-center">Payment Details</div>
-      <FontAwesomeIcon icon={faAngleRight} className="mx-5 self-center" />
-      <div className="mx-5 self-center">Summary</div>
-      <FontAwesomeIcon icon={faAngleRight} className="mx-5 self-center" />
-      <div className="mx-5 self-center">Confirmation</div>
+    <div className="mb-6 flex text-[18px] font-[300] leading-[24px]">
+      <div className={cn(tabElement, 'rounded-full bg-[#A2AAB6] px-4 py-1 text-[#333F4C] shadow-md')}>Order</div>
+      <FontAwesomeIcon icon={faAngleRight} className={cn(tabElement, 'text-[15px]')} />
+      <div className={cn(tabElement)}>Payment Details</div>
+      <FontAwesomeIcon icon={faAngleRight} className={cn(tabElement, 'text-[15px]')} />
+      <div className={cn(tabElement)}>Summary</div>
+      <FontAwesomeIcon icon={faAngleRight} className={cn(tabElement, 'text-[15px]')} />
+      <div className={cn(tabElement)}>Confirmation</div>
     </div>
   );
 };
