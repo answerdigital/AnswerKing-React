126eae4b463cffc9f9ff68e927736057e8fef42f
Harry Stead <Harry.Stead@mft.nhs.uk>
2023-01-31 15:05:58 +0000

    BENCH-355: Carousel added for categories

--- src/components/MenuCategories/MenuCategories.tsx
+++ src/components/MenuCategories/MenuCategories.tsx
@@ -1,7 +1,11 @@
 import { CategoryDto } from 'dtos/CategoryDto';
-import { Dispatch, ReactElement, SetStateAction } from 'react';
+import { Dispatch, ReactElement, SetStateAction, useState } from 'react';
 import { Button } from 'components/Button/Button';
+import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { AnimateSharedLayout, motion } from 'framer-motion';
 import cn from 'classnames';
+
 interface Props {
   setSelectedCategory: Dispatch<SetStateAction<number>>;
   selectedCategory: number;
@@ -9,28 +13,53 @@ interface Props {
 }
 
 export const MenuCategories = ({ categories, setSelectedCategory, selectedCategory }: Props): ReactElement => {
+  const [index, setIndex] = useState(0);
+
+  const handleNext = (): void => {
+    setIndex((prevIndex) => (prevIndex + 5 >= categories.length ? 0 : prevIndex + 5));
+  };
+
+  const handlePrev = (): void => {
+    setIndex((prevIndex) => (prevIndex - 5 < 0 ? prevIndex : prevIndex - 5));
+  };
+
   return (
     <div className="mt-5 flex flex-col items-center">
       <p className="text-[36px] font-[300]">Menu</p>
       <div className="mt-5 w-[95%] divide-y-2 divide-slate-700">
-        <div>
-          {categories.map(
-            (category) =>
-              !category?.retired && (
-                <Button
-                  key={category.id}
-                  size="medium"
-                  colour="clear"
-                  className={cn(
-                    'mx-[20px] h-[32px] w-[102px] bg-transparent py-0.5 px-5 text-[16px] font-[300] text-[#ffffff]',
-                    category.id === selectedCategory && 'border-[#333F4C] bg-[#A2AAB6] text-gray-900'
-                  )}
-                  onClick={() => setSelectedCategory(category.id)}
-                >
-                  {category.name}
-                </Button>
-              )
-          )}
+        <div className="flex items-center justify-between">
+          <div
+            className="my-auto flex h-[30px] w-[30px] items-center justify-center rounded-full border border-[#ffffff] text-center"
+            onClick={handlePrev}
+          >
+            <FontAwesomeIcon icon={faArrowLeft} className={cn('cursor-pointer text-[15px] text-[#ffffff]')} />
+          </div>
+          <div className="transition-transform duration-500 ease-out">
+            {categories.slice(index, index + 5).map(
+              (category, index) =>
+                category?.retired === false && (
+                  <Button
+                    key={index}
+                    size="medium"
+                    colour="clear"
+                    className={cn(
+                      'left-5 mx-[20px] h-[32px] w-[102px]',
+                      'bg-transparent px-5 text-[16px] font-[300] text-[#ffffff] transition-all duration-300 ease-in-out',
+                      category.id === selectedCategory && 'border-[#333F4C] bg-[#A2AAB6] text-gray-900'
+                    )}
+                    onClick={() => setSelectedCategory(category.id)}
+                  >
+                    {category.name}
+                  </Button>
+                )
+            )}
+          </div>
+          <div
+            className="my-auto flex h-[30px] w-[30px] items-center justify-center rounded-full border border-[#ffffff] text-center"
+            onClick={handleNext}
+          >
+            <FontAwesomeIcon icon={faArrowRight} className={cn('cursor-pointer text-[15px] text-[#ffffff]')} />
+          </div>
         </div>
         <div className="mt-5"></div>
       </div>
