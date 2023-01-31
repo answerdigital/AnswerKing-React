b02637f7a00e9758f43161a7ee9c561126468100
Harry Stead <Harry.Stead@mft.nhs.uk>
2023-01-31 09:06:57 +0000

    BENCH-346: Removed key

--- src/components/Transitions/ComponentTransition.tsx
+++ src/components/Transitions/ComponentTransition.tsx
@@ -10,7 +10,7 @@ export const ComponentTransition = (props: Props): ReactElement => {
   return (
     <AnimatePresence>
       {!props.lineItemsExist && (
-        <motion.div key="1" initial={{ opacity: 0, scale: 1 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 1 }} exit={{ opacity: 0 }}>
+        <motion.div initial={{ opacity: 0, scale: 1 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 1 }} exit={{ opacity: 0 }}>
           {props.children}
         </motion.div>
       )}
