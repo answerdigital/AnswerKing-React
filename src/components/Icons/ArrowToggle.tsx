57b18854384c94be9923db418bdec21c41293581
Harry Stead <Harry.Stead@mft.nhs.uk>
2023-02-01 13:53:22 +0000

    BENCH-355: Review comments implemented

--- src/components/Icons/ArrowToggle.tsx
+++ src/components/Icons/ArrowToggle.tsx
@@ -2,20 +2,29 @@ import { ReactElement } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import cn from 'classnames';
 import { IconDefinition } from '@fortawesome/fontawesome-common-types';
+import { CategoryDto } from 'dtos/CategoryDto';
 
 interface Props {
   icon: IconDefinition;
   handleIndex: () => void;
+  categories: CategoryDto[];
+  maxItemsDisplayed: number;
 }
 
-export const ArrowToggle = ({ icon, handleIndex }: Props): ReactElement => {
+export const ArrowToggle = ({ icon, handleIndex, categories, maxItemsDisplayed }: Props): ReactElement => {
+  const moreThan4Categories =
+    categories.filter((category) => !category?.retired === false || category.products?.length !== 0).length > maxItemsDisplayed;
   return (
-    <div
-      className="z-10 my-auto flex h-[25px] w-[25px] cursor-pointer items-center justify-center rounded-full border border-[#ffffff]
+    <>
+      {moreThan4Categories && (
+        <div
+          className="z-10 my-auto flex h-[25px] w-[25px] cursor-pointer items-center justify-center rounded-full border border-[#ffffff]
     text-center hover:cursor-pointer group-hover:border-[#E4EAEB]"
-      onClick={handleIndex}
-    >
-      <FontAwesomeIcon icon={icon} className={cn('text-[13px] text-[#ffffff] group-hover:text-[#E4EAEB]')} />
-    </div>
+          onClick={handleIndex}
+        >
+          <FontAwesomeIcon icon={icon} className={cn('text-[13px] text-[#ffffff] group-hover:text-[#E4EAEB]')} />
+        </div>
+      )}
+    </>
   );
 };
--- src/components/MenuCategories/MenuCategories.tsx
+++ src/components/MenuCategories/MenuCategories.tsx
@@ -1,6 +1,5 @@
 import { CategoryDto } from 'dtos/CategoryDto';
 import { Dispatch, ReactElement, SetStateAction, useState } from 'react';
-import { Button } from 'components/Button/Button';
 import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import cn from 'classnames';
 import { motion } from 'framer-motion';
@@ -14,44 +13,47 @@ interface Props {
 
 export const MenuCategories = ({ categories, setSelectedCategory, selectedCategory }: Props): ReactElement => {
   const [index, setIndex] = useState(0);
+  const [direction, setDirection] = useState('');
+  const maxItemsDisplayed = 4;
 
   const handleNext = (): void => {
-    setIndex((prevIndex) => (prevIndex + 5 >= categories.length ? 0 : prevIndex + 5));
+    setIndex((prevIndex) => (prevIndex + maxItemsDisplayed >= categories.length ? 0 : prevIndex + maxItemsDisplayed));
+    setDirection('left');
   };
 
   const handlePrev = (): void => {
-    setIndex((prevIndex) => (prevIndex - 5 < 0 ? prevIndex : prevIndex - 5));
+    setIndex((prevIndex) => (prevIndex - maxItemsDisplayed < 0 ? prevIndex : prevIndex - maxItemsDisplayed));
+    setDirection('right');
   };
 
   return (
     <div className="mt-5 flex flex-col items-center">
       <p className="text-[36px] font-[300]">Menu</p>
       <div className="mt-5 w-[95%] divide-y-2 divide-slate-700">
-        <div className="group flex items-center justify-between">
-          <ArrowToggle icon={faArrowLeft} handleIndex={handlePrev} />
-          {categories.slice(index, index + 5).map(
+        <div className="group flex items-center justify-between text-center">
+          <ArrowToggle icon={faArrowLeft} handleIndex={handlePrev} categories={categories} maxItemsDisplayed={maxItemsDisplayed} />
+          {categories.slice(index, index + maxItemsDisplayed).map(
             (category) =>
-              category?.retired === false && (
-                <motion.button key={category.id} initial={{ opacity: 0.2, x: 50 }} animate={{ opacity: 1, x: 0, transition: { duration: 1 } }}>
-                  <Button
-                    key={category.id}
-                    size="medium"
-                    colour="clear"
-                    className={cn(
-                      'left-5 mx-[20px] h-[32px] w-[102px]',
-                      'bg-transparent px-5 text-[16px] font-[300] text-[#ffffff]',
-                      category.id === selectedCategory && 'border-[#333F4C] bg-[#A2AAB6] text-gray-900'
-                    )}
-                    onClick={() => {
-                      setSelectedCategory(category.id);
-                    }}
-                  >
-                    {category.name}
-                  </Button>
+              !category?.retired === false ||
+              (category.products?.length !== 0 && (
+                <motion.button
+                  key={category.id}
+                  initial={{ opacity: 0.2, x: direction == 'left' ? 50 : -50 }}
+                  animate={{ opacity: 1, x: 0, transition: { duration: 1 } }}
+                  className={cn(
+                    'left-5 mx-[20px] h-[32px] w-[102px] rounded-full',
+                    'bg-transparent px-1 text-[16px] font-[300] text-[#ffffff] hover:border-[#333F4C] hover:bg-[#333F4C]',
+                    category.id === selectedCategory && 'border-[#333F4C] bg-[#A2AAB6] text-gray-900'
+                  )}
+                  onClick={() => {
+                    setSelectedCategory(category.id);
+                  }}
+                >
+                  {category.name}
                 </motion.button>
-              )
+              ))
           )}
-          <ArrowToggle icon={faArrowRight} handleIndex={handleNext} />
+          <ArrowToggle icon={faArrowRight} handleIndex={handleNext} categories={categories} maxItemsDisplayed={maxItemsDisplayed} />
         </div>
         <div className="mt-5"></div>
       </div>
