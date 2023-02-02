eca5e257ff4a01298efbe358b38a1a97f2639681
Mindaugas Baltrimas <104827741+mbaltrimas@users.noreply.github.com>
2023-02-02 13:51:18 +0000

    BENCH-317: Create Allergies Page (#108)
    * BENCH-317 Allergens
    
    * Menu modal func added
    
    * Added styling and updated counter
    
    * prettier run
    
    * fixed code smells
    
    * prettier run again
    
    * fix sonercloud bug
    
    * PR changes
    
    * run prettier yet again
    
    * fixed styling
    
    * first iteration of allergen page, still fighting with the table header background
    
    * sticky table header was too strong, I gave up and it no longer is sticky
    
    * remove comment, use keys
    
    * update snapshot
    
    * restore tailwind
    
    * relax, sonar
    
    * sonar doesnt like array indexes
    
    * remove duplicate product.name uses
    
    * create allergentable, remove space :)
    
    * eslint told me to add a return type
    
    * nest th in a tr
    
    ---------
    
    Co-authored-by: AudriusPutys <Audrius.Putys@answerdigital.com>
    Co-authored-by: NogaAD <115239141+NogaAD@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import { RouteConstants } from 'utilities/route-constants';
 import { HelmetProvider } from 'react-helmet-async';
 import { LocalOrderProvider } from '.../../context/OrderContext';
 import { AnimatePresence } from 'framer-motion';
+import { AllergensPage } from './pages/Allergens/Allergens';
 
 const queryClient = new QueryClient();
 
@@ -26,6 +27,7 @@ export const App = (): ReactElement => {
                 <Route element={<StaffPage />} path={RouteConstants.STAFF} />
                 <Route element={<HomePage />} path={RouteConstants.HOME} />
                 <Route element={<CheckoutPage />} path={RouteConstants.CHECKOUT} />
+                <Route element={<AllergensPage />} path={RouteConstants.ALLERGEN_BOARD} />
               </Routes>
             </AnimatePresence>
           </Layout>
