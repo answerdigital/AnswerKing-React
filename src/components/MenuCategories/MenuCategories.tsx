b91b6e450b6b50d570423ba7d207e87a8133ceb0
Harry Stead <Harry.Stead@mft.nhs.uk>
2023-02-03 14:42:42 +0000

    BENCH-383

--- src/components/MenuCategories/MenuCategories.tsx
+++ src/components/MenuCategories/MenuCategories.tsx
@@ -28,7 +28,7 @@ export const MenuCategories = ({ categories, setSelectedCategory, selectedCatego
   };
 
   return (
-    <div className="mt-5 flex flex-col items-center">
+    <div className="mt-8 flex flex-col items-center">
       <p className="text-[36px] font-[300]">Menu</p>
       <div className="mt-5 w-[95%] divide-y-2 divide-slate-700">
         <div className="group flex items-center justify-between text-center">
