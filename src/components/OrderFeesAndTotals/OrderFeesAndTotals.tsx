3c267c9fd31efce0f4d21e1477a90232de63dad9
mbaltrimas <mindaugas.baltrimas@answerdigital.com>
2023-02-07 11:34:00 +0000

    remove asdasd :)

--- src/components/OrderFeesAndTotals/OrderFeesAndTotals.tsx
+++ src/components/OrderFeesAndTotals/OrderFeesAndTotals.tsx
@@ -21,7 +21,7 @@ export const OrderFeesAndTotals = ({ lineItems, orderComplete }: Props): React.R
       )}
 
       <div className={cn('mt-2 mb-4 flex w-full justify-between text-[20px] font-[600] text-[#333F4C]')}>
-        <span>{orderComplete ? 'Paid' : 'Totalasdasd'}:</span>
+        <span>{orderComplete ? 'Paid' : 'Total'}:</span>
         <span>{GBPFormat.format(lineItems.length > 0 ? lineItems.reduce((partialSum, a) => partialSum + a.subTotal, 0) + SERVICE_CHARGE : 0)}</span>
       </div>
     </div>
