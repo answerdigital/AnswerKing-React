fae60d328b808ea5c929b22c0862f9ef087dbbfa
Mindaugas Baltrimas <104827741+mbaltrimas@users.noreply.github.com>
2023-02-21 13:58:32 +0000

    BENCH-440: Fix checkboxes not registering values in RHF (#132)
    * BENCH-440: Fix checkboxes not registering values in RHF
    
    * use checked/defaultchecked values

--- src/components/Inputs/Checkbox.tsx
+++ src/components/Inputs/Checkbox.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, useState } from 'react';
+import React, { forwardRef, useCallback, useImperativeHandle, useRef, useState } from 'react';
 import cn from 'classnames';
 import { CheckBoxIcon } from 'components/Icons/CheckBoxIcon';
 
@@ -6,13 +6,28 @@ interface Props extends React.ComponentPropsWithRef<'input'> {
   label?: string;
 }
 
-export const Checkbox = forwardRef<HTMLInputElement, Props>(({ label, className, id, ...rest }, ref) => {
-  const [checked, setChecked] = useState<boolean>(false);
+export const Checkbox = forwardRef<HTMLInputElement, Props>(({ label, className, checked, defaultChecked, id, ...rest }, ref) => {
+  const innerRef = useRef<HTMLInputElement>(null);
+  const [isChecked, setIsChecked] = useState<boolean>(checked ?? defaultChecked ?? false);
+
+  useImperativeHandle(ref, () => innerRef.current as HTMLInputElement);
+
+  const handleChecked = useCallback(
+    (value: boolean) => {
+      const checkbox = innerRef.current;
+
+      if (checkbox) {
+        setIsChecked(value);
+        checkbox.click();
+      }
+    },
+    [innerRef]
+  );
 
   return (
-    <div className={cn('flex w-fit cursor-pointer select-none items-center gap-2', className)} onClick={() => setChecked(!checked)}>
-      <input className="hidden" type="checkbox" checked={checked} id={id} ref={ref} {...rest}></input>
-      <CheckBoxIcon checked={checked} />
+    <div className={cn('flex w-fit cursor-pointer select-none items-center gap-2', className)} onClick={() => handleChecked(!isChecked)}>
+      <input className="hidden" type="checkbox" id={id} ref={innerRef} {...rest}></input>
+      <CheckBoxIcon checked={isChecked} />
       <span>{label}</span>
     </div>
   );
