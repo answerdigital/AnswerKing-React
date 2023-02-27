500fabbd0340c3cb1568f27b36c1eb5d096c39b9
Harry Stead <Harry.Stead@mft.nhs.uk>
2023-02-27 11:20:29 +0000

    BENCH-439: run prettier

--- src/hooks/useCategories.ts
+++ src/hooks/useCategories.ts
@@ -7,8 +7,9 @@ interface UseCategoriesResult {
 }
 
 export default function useCategories(filtered = false): UseCategoriesResult {
-  const categories = useQuery<CategoryDto[]>(['categories'], CategoryService.getAll,
-    {select: (data) => (filtered ? data.filter((category) => !category.retired || (category.products && category.products.length > 0)) : data)});
+  const categories = useQuery<CategoryDto[]>(['categories'], CategoryService.getAll, {
+    select: (data) => (filtered ? data.filter((category) => !category.retired || (category.products && category.products.length > 0)) : data),
+  });
 
   return { categories };
 }
